c589927d1619b2ac3ca4c13b404f9546864ca163
cs_236350 <ubuntu@ubuntu2004.linuxvmimages.local>
2023-06-10 05:02:36 -0400

    add basic filtering in order to fine license plate

--- license-plate-recognition/lpr.py
+++ license-plate-recognition/lpr.py
@@ -9,7 +9,6 @@ import matplotlib.patches as patches
 
 
 car_image = imread("test_img/car.jpeg", as_gray=True)
-
 gray_car_image = car_image * 255
 fig, (ax1, ax2) = plt.subplots(1, 2)
 ax1.imshow(gray_car_image, cmap="gray")
@@ -17,24 +16,51 @@ ax1.imshow(gray_car_image, cmap="gray")
 # get threshold to distinct foreground an background using otsu method
 threshold_value = threshold_otsu(gray_car_image)
 binary_car_image = gray_car_image > threshold_value
+
+
 ax2.imshow(binary_car_image, cmap="gray")
 plt.show()
 
 # this gets all the connected regions and groups them together
 label_image = measure.label(binary_car_image)
+# getting the maximum width, height and minimum width and height that a license plate can be
+plate_dimensions = (0.05*label_image.shape[0], 0.2*label_image.shape[0], 0.15*label_image.shape[1], 0.4*label_image.shape[1])
+min_height, max_height, min_width, max_width = plate_dimensions
+plate_objects_cordinates = []
+plate_like_objects = []
 fig, (ax1) = plt.subplots(1)
 ax1.imshow(gray_car_image, cmap="gray");
-
+print(str(min_height) + " = min_height")
+print(str(max_height) + " = max_height")
+print(str(min_width) + " = min_width")
+print(str(max_width) + " = max_width")
 # regionprops creates a list of properties of all the labelled regions
 for region in regionprops(label_image):
     if region.area < 50:
         #if the region is so small then it's likely not a license plate
         continue
 
+    if region.area > car_image.shape[0]*car_image.shape[1]*0.5:
+        #If the region is too big its probably not the licesnse plate
+        continue
     # the bounding box coordinates
-    minRow, minCol, maxRow, maxCol = region.bbox
-    rectBorder = patches.Rectangle((minCol, minRow), maxCol-minCol, maxRow-minRow, edgecolor="yellow", linewidth=3, fill=False)
-    ax1.add_patch(rectBorder)
+    min_row, min_col, max_row, max_col = region.bbox
+    region_height = max_row - min_row
+    region_width = max_col - min_col
+    # ensuring that the region identified satisfies the condition of a typical license plate
+    print(str(region_height) + " = region_height")
+
+    print(str(region_width) + " = region_width")
+
+
+    if region_height >= min_height and region_height <= max_height and region_width >= min_width and region_width <= max_width and region_width > region_height:
+        print("asdsa")
+        plate_like_objects.append(binary_car_image[min_row:max_row,
+                                  min_col:max_col])
+        plate_objects_cordinates.append((min_row, min_col,
+                                              max_row, max_col))
+        rectBorder = patches.Rectangle((min_col, min_row), max_col-min_col, max_row-min_row, edgecolor="red", linewidth=2, fill=False)
+        ax1.add_patch(rectBorder)
     # let's draw a red rectangle over those regions
 
 plt.show()
